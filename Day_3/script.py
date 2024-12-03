9b472b1135b9fdda77fd114671ccae1527e50b93
François MERCADAL <mercadal.franois7@gmail.com>
2024-12-04 00:55:54 +0100

    Day 3 (script + input + readme)

---
+++ Day_3/script.py
@@ -0,0 +1,56 @@
+import regex as re
+
+FILE_NAME = "memory.txt"
+
+# PART 1
+def get_mul_sum_p1(file):
+    mul_sum = 0
+    for line in file:
+        line = line.strip()
+        mul_array = re.findall('mul\([0-9]+,[0-9]+\)', line)
+        mul_array = [re.findall('[0-9]+,[0-9]+', x)[0] for x in mul_array]
+
+        for mul in mul_array:
+            mul = mul.split(",")
+            mul = [int(x) for x in mul]
+            mul_sum += mul[0]*mul[1]
+    return mul_sum
+
+def mull_it_over_part1():
+    file = open(FILE_NAME, "r")
+    mul_sum = get_mul_sum_p1(file)
+    file.close()
+    return mul_sum
+
+# PART 2
+def get_mul_sum_p2(file):
+    mul_enabled = True
+    mul_sum = 0
+    for line in file:
+        line = line.strip()
+        mul_array = re.findall("mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)", line)
+        for item in mul_array:
+            if item == "do()":
+                mul_enabled = True
+            elif item == "don't()":
+                mul_enabled = False
+            else:
+                if mul_enabled == True:
+                    item = re.findall('[0-9]+,[0-9]+', item)[0].split(",")
+                    item = [int(x) for x in item]
+                    mul_sum += item[0]*item[1]
+    return mul_sum
+
+def mull_it_over_part2():
+    file = open(FILE_NAME, "r")
+    mul_sum = get_mul_sum_p2(file)
+    file.close()
+    return mul_sum
+
+# Part 1
+print(mull_it_over_part1())
+# Output: 184576302
+
+# Part 2
+print(mull_it_over_part2())
+# Output: 118173507
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Advent_of_Code_2024
-## Stars collected : 2
+## Stars collected : 6
 * Day 1: https://adventofcode.com/2024/day/1 (2*)
 * Day 2: https://adventofcode.com/2024/day/2 (2*)
+* Day 3: https://adventofcode.com/2024/day/3 (2*)
