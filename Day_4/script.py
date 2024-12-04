da105be81e18c987a4e7594f359b63556fcb9c4a
François MERCADAL <mercadal.franois7@gmail.com>
2024-12-04 23:43:56 +0100

    Day 4 (script + input + ReadMe)

---
+++ Day_4/script.py
@@ -0,0 +1,94 @@
+FILE_NAME = "words_input.txt"
+WORD_TO_SEARCH = 'XMAS'
+
+# PART 1
+def horizontal_search(lines, line, col):
+    nb = 0
+    # Forward
+    if col <= len(lines[line]) - len(WORD_TO_SEARCH):
+        if lines[line][col] + lines[line][col+1] + lines[line][col+2] + lines[line][col+3] == WORD_TO_SEARCH:
+            nb += 1
+    # Backward
+    if col >= len(WORD_TO_SEARCH) - 1:
+        if lines[line][col] + lines[line][col-1] + lines[line][col-2] + lines[line][col-3] == WORD_TO_SEARCH:
+            nb += 1
+    return nb
+
+def vertical_search(lines, line, col):
+    nb = 0
+    # Up
+    if line >= len(WORD_TO_SEARCH) - 1:
+        if lines[line][col] + lines[line-1][col] + lines[line-2][col] + lines[line-3][col] == WORD_TO_SEARCH:
+            nb += 1
+    # Down
+    if line <= len(lines) - len(WORD_TO_SEARCH):
+        if lines[line][col] + lines[line+1][col] + lines[line+2][col] + lines[line+3][col] == WORD_TO_SEARCH:
+            nb += 1
+    return nb
+
+def diagonal_search(lines, line, col):
+    nb = 0
+    # Up, Forward
+    if line >= len(WORD_TO_SEARCH) - 1 and col <= len(lines[line]) - len(WORD_TO_SEARCH):
+        if lines[line][col] + lines[line-1][col+1] + lines[line-2][col+2] + lines[line-3][col+3] == WORD_TO_SEARCH:
+            nb += 1
+    # Up, Backward
+    if line >= len(WORD_TO_SEARCH) - 1 and col >= len(WORD_TO_SEARCH) - 1:
+        if lines[line][col] + lines[line-1][col-1] + lines[line-2][col-2] + lines[line-3][col-3] == WORD_TO_SEARCH:
+            nb += 1
+    # Down, Forward
+    if line <= len(lines) - len(WORD_TO_SEARCH) and col <= len(lines[line]) - len(WORD_TO_SEARCH):
+        if lines[line][col] + lines[line+1][col+1] + lines[line+2][col+2] + lines[line+3][col+3] == WORD_TO_SEARCH:
+            nb += 1
+    # Down, Backward
+    if line <= len(lines) - len(WORD_TO_SEARCH) and col >= len(WORD_TO_SEARCH) - 1:
+        if lines[line][col] + lines[line+1][col-1] + lines[line+2][col-2] + lines[line+3][col-3] == WORD_TO_SEARCH:
+            nb += 1
+    return nb
+
+def find_patterns_p1(file):
+    nb_words_found = 0
+    lines = file.readlines()
+    lines = [line.strip() for line in lines]
+
+    for line in range(len(lines)):
+        for col in range(len(lines[line])):
+            if lines[line][col] == WORD_TO_SEARCH[0]:
+                nb_words_found += horizontal_search(lines, line, col)
+                nb_words_found += vertical_search(lines, line, col)
+                nb_words_found += diagonal_search(lines, line, col)
+    return nb_words_found
+
+def ceres_search_part1():
+    file = open(FILE_NAME, "r")
+    nb_words_found = find_patterns_p1(file)
+    file.close()
+    return nb_words_found
+
+# PART 2
+def find_patterns_p2(file):
+    nb_words_found = 0
+    lines = file.readlines()
+    lines = [line.strip() for line in lines]
+
+    for line in range(len(lines)):
+        for col in range(len(lines[line])):
+            if lines[line][col] == "A" and col >= 1 and col <= len(lines[line]) - 2 and line >= 1 and line <= len(lines) - 2:
+                list_char = [lines[line-1][col-1],lines[line-1][col+1],lines[line+1][col-1],lines[line+1][col+1]]
+                if list_char.count("S") == 2 and list_char.count("M") == 2 and (lines[line-1][col-1] == lines[line-1][col+1] or lines[line-1][col-1] == lines[line+1][col-1]):
+                    nb_words_found += 1
+    return nb_words_found
+
+def ceres_search_part2():
+    file = open(FILE_NAME, "r")
+    nb_words_found = find_patterns_p2(file)
+    file.close()
+    return nb_words_found
+
+# Part 1
+print(ceres_search_part1())
+# Output: 2507
+
+# Part 2
+print(ceres_search_part2())
+# Output: 1969
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Advent_of_Code_2024
-## Stars collected : 6
+## Stars collected : 8
 * Day 1: https://adventofcode.com/2024/day/1 (2*)
 * Day 2: https://adventofcode.com/2024/day/2 (2*)
 * Day 3: https://adventofcode.com/2024/day/3 (2*)
+* Day 4: https://adventofcode.com/2024/day/4 (2*)
